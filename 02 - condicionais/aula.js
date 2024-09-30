11f5069e7571c2d0454eaa9188f05338d7042bac
tulioperdigao <116309232+tulioperdigao@users.noreply.github.com>
2024-09-30 15:12:05 -0300

    else if

--- 02 - condicionais/aula.js
+++ 02 - condicionais/aula.js
@@ -1,11 +1,21 @@
-const numero = 4; // = para atribuição
+const numero = 5; // = para atribuição
 
 const numeroDivisivelPor5 = (numero % 5) === 0; // == igualdade (ignora o tipo da variável)
 
-if (numeroDivisivelPor5) {
-    console.log('Sim')
-} 
+//condicionais tem que
+
+if (numero === 0) {
+    console.log ('Número inserido é inválido');
+}
+
+else if (numero === 5) {
+    console.log('Número inválido');
+}
+
+else if (numeroDivisivelPor5) {
+    console.log('Sim');
+}
 
 else {
-    console.log('Não')
+    console.log('Não');
 }
