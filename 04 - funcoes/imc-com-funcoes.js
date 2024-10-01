8f0c1affe1c3b50ef6d6aa69ffcaf4ecb6e2cd9a
tulioperdigao <116309232+tulioperdigao@users.noreply.github.com>
2024-10-01 15:47:47 -0300

    Create imc-com-funcoes.js

---
+++ 04 - funcoes/imc-com-funcoes.js
@@ -0,0 +1,28 @@
+function calcularImc(peso, altura) {
+    return peso / Math.pow(altura, 2);
+}
+
+function classificarImc(imc) {
+    if (imc < 18.5) {
+        return 'Abaixo do peso';
+    } else if (imc < 25) {
+        return 'Peso normal';
+    } else if (imc < 30) {
+        return 'Acima do peso';
+    } else if (imc < 40) {
+        return 'Obeso';
+    } else {
+        return 'Obesidade Grave';
+    }
+}
+
+function main() {
+    const peso = 110;
+    const altura = 1.65;
+    const imc = calcularImc(peso, altura);
+
+    console.log(imc)
+    console.log(classificarImc(imc));
+}
+
+main()
