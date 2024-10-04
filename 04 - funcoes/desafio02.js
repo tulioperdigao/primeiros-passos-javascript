3fbbc9ba50b336d134814ad7e4de787be951a8dc
tulioperdigao <116309232+tulioperdigao@users.noreply.github.com>
2024-10-04 15:31:54 -0300

    Create desafio02.js

---
+++ 04 - funcoes/desafio02.js
@@ -0,0 +1,11 @@
+function verificarIdade(idade) {
+    if (idade < 18) {
+        console.log('É menor de idade.');
+    }
+
+    else {
+        console.log('É maior de idade.');
+    }
+}
+
+verificarIdade(18);
