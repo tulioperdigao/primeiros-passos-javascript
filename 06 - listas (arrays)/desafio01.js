5f6735d00e2532b5f3d2cb6344f7a5e92bce5a40
tulioperdigao <116309232+tulioperdigao@users.noreply.github.com>
2024-10-08 14:44:57 -0300

    desafio 02

--- 06 - listas (arrays)/desafio01.js
+++ 06 - listas (arrays)/desafio01.js
@@ -2,6 +2,6 @@ let numero = 10;
 let resultado = 0;
 
 for (let i = 1; i <= 10 ; i++) {
-    resultado = numero * i
+    resultado = numero * i;
     console.log(`${numero} x ${i} = ${resultado}`);
 }
