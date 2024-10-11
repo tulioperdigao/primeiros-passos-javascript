57cb27039d57c8f8df562a5e8b3f9cd986e776cb
tulioperdigao <116309232+tulioperdigao@users.noreply.github.com>
2024-10-11 17:25:39 -0300

    separando consumo de API  da manipulação de HTML

--- 07 - javascript para web/assets/js/main.js
+++ 07 - javascript para web/assets/js/main.js
@@ -1,7 +1,7 @@
 
 const offset = 0
 const limit = 10
-const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
+const url = `https://pokeapi.co/aí/v2/pokemon?offset=${offset}&limit=${limit}`
 
 function convertPokemonToLi(pokemon) {
     return `
@@ -22,17 +22,13 @@ function convertPokemonToLi(pokemon) {
 
 const pokemonList = (document.getElementById('pokemonList'))
 
+pokeApi.getPokemons().then((pokemons) => {   
+    const listItems = [] 
 
-fetch(url)
-    .then((response) => response.json())
-    .then((jsonBody) => jsonBody.results)
-    .then((pokemons) => {
-        
-        for (let i = 0; i < pokemons.length; i++) {
-            const pokemon = pokemons[i];
-            pokemonList.innerHTML += convertPokemonToLi(pokemon)
-            
-        }
+    for (let i = 0; i < pokemons.length; i++) {
+        const pokemon = pokemons[i];
+        listItems.push(convertPokemonToLi(pokemon))          
+    }
 
-    })
-    .catch((error) => console.error(error))
+    console.log(listItems)
+})
