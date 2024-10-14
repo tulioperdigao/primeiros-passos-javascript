46644e49bb25fa6833df34ed2bee22f485dea60f
Tulio Perdigão <116309232+tulioperdigao@users.noreply.github.com>
2024-10-14 16:42:32 -0300

    Adicionando tipos de pokemon dinamicamente

--- 07 - javascript para web/assets/js/poke-api.js
+++ 07 - javascript para web/assets/js/poke-api.js
@@ -23,7 +23,7 @@ pokeApi.getPokemonsDetail = (pokemon) => {
         .then(convertPokeApiDetailToPokemon)
 }
 
-pokeApi.getPokemons = (offset = 0, limit = 10) => {
+pokeApi.getPokemons = (offset = 0, limit = 5) => {
     const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
     return fetch(url)
         .then((response) => response.json())
