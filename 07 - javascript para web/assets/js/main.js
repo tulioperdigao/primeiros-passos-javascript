d1ddea0baa7070f28e98f86f66b2236df9adbecb
Tulio Perdigão <116309232+tulioperdigao@users.noreply.github.com>
2024-10-14 20:27:00 -0300

    aprimorando e limitando a paginação

--- 07 - javascript para web/assets/js/main.js
+++ 07 - javascript para web/assets/js/main.js
@@ -1,6 +1,8 @@
 const pokemonList = document.getElementById('pokemonList')
 const loadMoreButton = document.getElementById('loadMoreButton')
-const limit = 5
+
+const maxRecords = 151
+const limit = 10
 let offset = 0
 
 function loadPokemonItens(offset, limit) {
@@ -27,5 +29,15 @@ loadPokemonItens(offset, limit)
 
 loadMoreButton.addEventListener('click', () => {
     offset += limit
-    loadPokemonItens(offset, limit)
+    const qtdRecordWithNextPage = offset + limit
+
+    if (qtdRecordWithNextPage >= maxRecords) {
+        const newLimit = maxRecords - offset
+        loadPokemonItens(offset, newLimit)
+
+        loadMoreButton.parentElement.removeChild(loadMoreButton)
+
+    } else {
+        loadPokemonItens(offset, limit)
+    }
 })
