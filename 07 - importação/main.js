8fa64e1ab1997824961c02629935714ccae6f5f3
tulioperdigao <116309232+tulioperdigao@users.noreply.github.com>
2024-10-28 11:41:38 -0300

    importação

---
+++ 07 - importação/main.js
@@ -0,0 +1,14 @@
+
+const {gets, print} = require('./funcoes-auxiliares');
+
+print(gets());
+
+/*
+const pessoa = {
+    nome: 'Tulio'
+}
+
+const { nome } = pessoa;
+
+const nome = pessoa.nome;
+*/
